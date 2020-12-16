1c70b1563792acb959662e4dac4c445eba4a558c
Ousmaneaba <ousmane.aymerou.ba@efrei.net>
2020-12-16 16:20:47 +0100

    push implementation

--- app.js
+++ app.js
@@ -15,5 +15,39 @@ app.get('/', (req, res) => {
 	res.render('index');
 });
 
+app.get('/tweets', (req, res) => {
+
+    console.log(req.query.message)
+
+	if(req.query.message == undefined || req.query.message == null || req.query.message == '' ){
+		res.render('index');
+	}
+	else{
+
+		let timestamp = new Date().getTime();
+		let fileName = timestamp + "_tmp.txt";
+		let scriptProcess = spawn('python', ['tweets-tf-idf.py', req.query.message, fileName]);
+		let result = "error";
+
+		 scriptProcess.on('close', (code) => {
+
+		  	fs.readFile(fileName, 'utf8', function (err,data) {
+
+		  	if(err != null){
+		  	  res.send(500)
+		  	}
+		  	else{
+		  	  result = data
+              res.render('index', {message: req.query.message, result: result});
+		  	}
+
+			});
+
+		  });
+
+	}
+
+});
+
 
 module.exports = app
