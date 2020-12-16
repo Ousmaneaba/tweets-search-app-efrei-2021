793b3cedea1adaa49d9349dfe3835ad30f05939c
christophil <christophil090318@gmail.com>
2020-12-16 04:30:06 +0100

    - update app port - add jenkis stages for test

--- __tests__/tests.spec.js
+++ __tests__/tests.spec.js
@@ -1,6 +1,4 @@
-const serverOnLocalhost = require("../src/tests/serverOnLocalhost");
 const serverReturnHtmlPage = require("../src/tests/serverReturnHtmlPage");
 
-describe("Server is on localhost", serverOnLocalhost);
 describe("Server is returning an html page", serverReturnHtmlPage);
 
--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
   host: "0.0.0.0",
-  port: 8080,
+  port: 8111,
 }
